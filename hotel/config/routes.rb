22e2d0478f9695581933c2c9b1edb6e54cf9b789
iwahori1018 <swimmer.si1018@gmail.com>
2019-08-04 15:06:57 +0900

    test

--- hotel/config/routes.rb
+++ hotel/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
-  get 'reserve' => "home#reserve"
-  get 'change' => "home#change"
-  get 'delete' => "home#delete"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
